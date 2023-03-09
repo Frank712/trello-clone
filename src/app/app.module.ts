6e5b99ffb3ab1fb149a23f68f173e7fa2000cf92
Frank712 <franksuriel_7@hotmail.com>
2023-03-08 23:59:10 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { BtnComponent } from './components/btn/btn.component';
+import { BoardsComponent } from './pages/boards/boards.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BoardComponent } from './pages/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BtnComponent,
+    BoardsComponent,
+    NavbarComponent,
+    BoardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    OverlayModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    CdkAccordionModule,
+    DragDropModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/boards/boards.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  faBox,
+  faWaveSquare,
+  faClock,
+  faAngleUp,
+  faAngleDown,
+  faHeart,
+  faBorderAll,
+  faUsers,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-boards',
+  templateUrl: './boards.component.html',
+})
+export class BoardsComponent implements OnInit {
+  faBox = faBox;
+  faTrello = faTrello;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll;
+  faUsers = faUsers;
+  faCog = faCog;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
